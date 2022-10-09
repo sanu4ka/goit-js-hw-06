b4f745d2fe88085b3bc6519f43dc0049002955ab
sanu4ka <sanu4ka1989@gmail.com>
2022-10-09 23:16:00 +0300

    1-3

--- js/task-01.js
+++ js/task-01.js
@@ -1,1 +1,10 @@
+const numbersOfCategories = document.querySelectorAll(".item");
+console.log("Number of categories: " + numbersOfCategories.length);
 
+numbersOfCategories.forEach((element) => {
+  const categoryName = element.querySelector("h2");
+  const numbersOfElements = element.querySelectorAll("li");
+
+  console.log("Category: " + categoryName.textContent);
+  console.log("Elements: " + numbersOfElements.length);
+});
--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,18 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const newItem = ingredients.map((ingredient) => {
+  const item = document.createElement("li");
+  item.textContent = ingredient;
+  item.classList.add("item");
+  return item;
+});
+
+const list = document.querySelector("#ingredients");
+list.append(...newItem);
