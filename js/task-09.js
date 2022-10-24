4276f4bbaddf6efc4bc9981d1fc5e83f35d7cba7
sanu4ka <sanu4ka1989@gmail.com>
2022-10-24 21:22:51 +0300

    9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+
+const button = document.querySelector(".change-color");
+const body = document.querySelector("body");
+const span = document.querySelector(".color");
+
+button.addEventListener("click", (event) => {
+  body.style.backgroundColor = getRandomHexColor();
+  span.textContent = getRandomHexColor();
+});
